37e81d753204402425d459e2ea91156cbe827612
Jan Hyka <jahyka@microsoft.com>
2023-01-17 06:56:34 +0100

    ! incorrect product material schematic naming in recipes

--- js/crafting_calc_interface.js
+++ js/crafting_calc_interface.js
@@ -124,7 +124,7 @@ function copyStringToClipboard(str) {
 var itemsAccordion, prices, recipes, german, schematicsPrices;
 
 const version = "1";
-const lastUpdateTime = "2023-01-16";
+const lastUpdateTime = "2023-01-17";
 document.getElementById("lastUpdateTime").innerHTML = lastUpdateTime;
 console.log("Crafting Calculator Updated On: " + lastUpdateTime)
 console.log("Crafting Calculator Profile Version: " + version)
